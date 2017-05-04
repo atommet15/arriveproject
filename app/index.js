63b1c55d75bddfa358e5acc2ab533131c7931596
Adam Metro <adam.r.metro@gmail.com>
2017-05-04 10:56:13 -0500

    environment setup

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// render the main entrypoint of the app into the dom
+ReactDOM.render(
+  <div>hello</div>, document.getElementById("App")
+);
